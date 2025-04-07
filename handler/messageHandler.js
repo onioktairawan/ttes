392237155af6d4b258ea00b9f9a87dd4fa61a7cb
onioktairawan <122547358+onioktairawan@users.noreply.github.com>
2025-04-07 22:36:25 +0700

    Update messageHandler.js

--- handler/messageHandler.js
+++ handler/messageHandler.js
@@ -1,1 +1,23 @@
+const { handleAutoResponse, startGame } = require('../utils');
+
+// Menangani pesan masuk
+function handleMessage(client, message) {
+    const { body, from, isGroupMsg } = message;
+
+    // Respon otomatis berdasarkan kata kunci
+    handleAutoResponse(client, message);
+
+    // Memulai game "susun kata" jika ada perintah
+    if (body.startsWith(`${prefix}susun-kata`)) {
+        startGame(client, from);
+    }
+
+    // Menangani pesan welcome (Jika grup sudah diatur welcome message-nya)
+    const welcomeMessages = loadWelcomeMessages();
+    if (isGroupMsg && welcomeMessages[from]) {
+        client.sendMessage(from, welcomeMessages[from]);
+    }
+}
+
+module.exports = { handleMessage };
 
